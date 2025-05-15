0d1f7dd6dc0da8906bb4b026f9ad4bc94041abcc
Stork2625 <163506708+Stork2625@users.noreply.github.com>
2025-05-15 15:34:42 +0700

    Initial commit

---
+++ ts/lession2/demo.js
@@ -0,0 +1,4 @@
+const sum = (x,y)=>{
+    return x + y;
+    }
+    console.log(">>> check sum",sum(5,19));
---
+++ ts/lession2/demo.ts
@@ -0,0 +1,7 @@
+const sum2 = (x:number, y:number)=>{
+    return x + y;
+}
+//Truong hop dung
+console.log(">>> check sum2 ", sum2(5,19));
+//Truong hop bao loi
+console.log(">>> check sum2 ", sum2(5,'19'));
