c11bd695b91cf9d6f77a1457ec968b50db7e79ef
Stork2625 <163506708+Stork2625@users.noreply.github.com>
2025-05-17 14:52:08 +0700

    Thêm thư mục reactbasic ngày 17/5/2025

---
+++ reactbasic/src/views/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.scss';
+import MyConponent from './Example/MyComponents';
+// Có hai loại: Class Component/ funtion component(function, arrow)
+// JSX
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        Cái lồn dcm này học hành đéo hiệu quả t cay vcl
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <MyConponent/>
+        <MyConponent></MyConponent>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ reactbasic/src/views/Example/MyComponents.js
@@ -0,0 +1,14 @@
+import React from "react";
+class MyConponent extends React.Component{
+    /* 
+    JSX
+    
+    
+    */
+    render(){
+        return(
+            <div>Dit con me tuc vai lon u hoc thi buon ngu</div>
+        )
+    }
+}
+export default MyConponent;
